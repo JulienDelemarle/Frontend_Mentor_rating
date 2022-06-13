2a68f7e64562e943ea09f6e680ccb1ebc6fc7553
JulienDelemarle <https://github.com/JulienDelemarle>
2022-06-13 14:50:42 +0200

    delete useless file

--- index.js
+++
@@ -1,8 +0,0 @@
-const ratingScore = document.querySelector(".rating__btn--btn");
-
-function addRating() {
-   const rating= ratingScore.innerHTML;
-   console.log(rating);
-}
-
-export default addRating;
